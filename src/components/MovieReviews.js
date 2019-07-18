cd7b6e443e16708f1775eef245bbb37d251a8e15
Matt Williams <matt.seongjun@gmail.com>
2019-07-18 12:37:41 -0400

    Evrything works. One test not passing for some reason

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
-// Code MovieReviews Here
+import React from "react"
+
+const renderReviews = (reviews) => {
+    return reviews.map(review => <li className="review">{review.headline}</li>)
+}
+
+
+function MovieReviews(props){
+    return (
+        <div className="review-list">
+            <ul>
+                {renderReviews(props.reviews)}
+            </ul>
+        </div>
+    )
+}
+
+export default MovieReviews
